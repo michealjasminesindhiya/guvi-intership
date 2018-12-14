813aaa4aa39e60bd6e271ab9116c6ee5004de3c6
michealjasminesindhiya <45868479+michealjasminesindhiya@users.noreply.github.com>
2018-12-14 12:57:32 +0530

    Create reg.js

---
+++ reg.js
@@ -0,0 +1,50 @@
+alidate form () {
+x=document set Element By Id("Password").value
+if(x=="){
+alert ("password must be filled");
+return false
+}
+
+
+function validate form () {
+x=document set Element By Id("repeat password").value
+if(x=="){
+alert ("repeat password must be filled");
+return false
+}
+}function validate form () {
+x=document set Element By Id("user name").value
+if(x=="){
+alert ("user name must be filled");
+return false
+}
+
+function validate form () {
+x=document set Element By Id("collage name").value
+if(x=="){
+alert ("collage name must be filled");
+return false
+}
+
+function validate form () {
+x=document set Element By Id("degree").value
+if(x=="){
+alert ("degree must be filled");
+return false
+}
+  
+function validate form () {
+x=document set Element By Id("language").value
+if(x=="){
+alert ("language must be filled");
+return false
+}
+
+function validate form () {
+x=document set Element By Id("email").value
+if(x=="){
+alert ("email must be filled");
+return false
+}
+
+function v
