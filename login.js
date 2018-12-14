a1be060c9649c5ad20abb07bf1fa34c0410eac71
michealjasminesindhiya <45868479+michealjasminesindhiya@users.noreply.github.com>
2018-12-14 12:58:56 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,13 @@
+function validate form () {
+x=document set Element By Id("email").value
+if(x=="){
+alert ("email must be filled");
+return false
+}
+
+function validate form () {
+x=document set Element By Id("Password").value
+if(x=="){
+alert ("password must be filled");
+return false
+}
